aed72ce66dfa7fb777733d460adf3ceaba5825c0
Steve Dusome <stevedusome@gmail.com>
2022-01-31 15:01:22 -0500

    finally got mvp

--- index.js
+++ index.js
@@ -113,10 +113,30 @@ function Program () {
     this.desiredUsers = 0;
     this.storedUsers = [];
     this.acceptingInput = 'Yes';
-    this.storedMarkdown = '';
+    this.storedMarkdown = '<!DOCTYPE html>' +
+    '<html lang="en">' +
+    '<head>' +
+        '<meta charset="UTF-8">' +
+        '<meta http-equiv="X-UA-Compatible" content="IE=edge">'+
+        '<meta name="viewport" content="width=device-width, initial-scale=1.0">' +
+        '<title>Team Members</title>' +
+        '<link rel="stylesheet" href="style.css">' +
+    '</head>' +
+    '<body>' +
+    
+        '<header>' +
+            '<h1>Your Team</h1>' +
+    
+        '</header>';
 }
 
-
+Program.prototype.writeFile = function () {
+    fs.appendFile('./dist/index.html', this.storedMarkdown, function(err)
+    {
+        if (err) throw err;
+        return console.log('File created')
+    })
+}
 
 Program.prototype.addMember = function() {
 return inquirer
@@ -132,7 +152,7 @@ return inquirer
             manager.officeNo = answers.office;
             this.acceptingInput = answers.continue;
             this.storedUsers.push(manager)
-        console.log(this.storedUsers)})}
+        })}
     
     else if (answers.role === 'Engineer'){
 
@@ -143,7 +163,6 @@ return inquirer
             engineer.github = answers.github;
             this.acceptingInput = answers.continue;
             this.storedUsers.push(engineer)
-        console.log(this.storedUsers)
         })
     }
     else if (answers.role === 'Intern') {
@@ -155,7 +174,6 @@ return inquirer
             intern.school = answers.school;
             this.acceptingInput = answers.continue;
             this.storedUsers.push(intern)
-        console.log(this.storedUsers)
         })
 
     }
@@ -167,7 +185,6 @@ return inquirer
             .then ((answers) => {
             this.acceptingInput = answers.continue;
             this.storedUsers.push(employee)
-        console.log(this.storedUsers)
         })
 
     }
@@ -183,19 +200,42 @@ return inquirer
 Program.prototype.inputLoop = async function() {
     while (this.acceptingInput === 'Yes') {
     let memberInput = await this.addMember();
-    console.log(this);
     console.log(memberInput);}
     this.generateMarkdown();
+    this.writeFile();
 }
 
 
 Program.prototype.generateMarkdown = function() {
     for (let i = 0; i < this.storedUsers.length; i++) {
-    this.storedMarkdown += "Name: " + this.storedUsers[i].name + ", "
+    this.storedMarkdown += "<div><h3> " + this.storedUsers[i].role + "</h3> " +
+    "<p>Name:" + this.storedUsers[i].name + "</p> " +
+    "<p>Id: " + this.storedUsers[i].id + "</p> " +
+    "<p>Email: <a href='mailto:" + this.storedUsers[i].email + "'>" + this.storedUsers[i].email + "</a> </p> "
 
-        
-      }
-    return console.log(this.storedMarkdown)
+    if (this.storedUsers[i].role === 'Manager')
+        { this.storedMarkdown += "<p>Office Number: " + this.storedUsers[i].officeNo + "</p> "
+
+        }
+    else if (this.storedUsers[i].role === 'Intern')
+        { this.storedMarkdown += "<p>School: " + this.storedUsers[i].school + "</p> "
+
+        }
+    else if (this.storedUsers[i].role === 'Engineer')
+        { this.storedMarkdown += "<p>github: <a href='https://github.com/" + this.storedUsers[i].github +  "'>" + 
+        this.storedUsers[i].github + "</a> </p>"
+        }
+
+    this.storedMarkdown += "</div>" +
+    "<footer>" +
+    "<h2 class=>Powered by Teamwork </h2>" +
+    "</footer>" +
+    "</body>" +
+    "</html>"
+    
+    ;        
+    }
+    return console.log("Please wait")
 }
 
 new Program().inputLoop()
